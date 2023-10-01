12a22a4b5b667c4733475dceb87d776bdfcb65b6
Aslan Ibragimov <aslan18asker@gmail.com>
2023-10-01 12:49:54 +0300

    !

---
+++ src/build/CMakeFiles/3dViewer.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "3dViewer_autogen"
+  "CMakeFiles/3dViewer_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/3dViewer_autogen.dir/ParseCache.txt"
+  "3dViewer"
+  "3dViewer.pdb"
+  "CMakeFiles/3dViewer.dir/3dViewer/GLViewer.cpp.o"
+  "CMakeFiles/3dViewer.dir/3dViewer/GLViewer.cpp.o.d"
+  "CMakeFiles/3dViewer.dir/3dViewer/main.cpp.o"
+  "CMakeFiles/3dViewer.dir/3dViewer/main.cpp.o.d"
+  "CMakeFiles/3dViewer.dir/3dViewer/mainwindow.cpp.o"
+  "CMakeFiles/3dViewer.dir/3dViewer/mainwindow.cpp.o.d"
+  "CMakeFiles/3dViewer.dir/3dViewer_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/3dViewer.dir/3dViewer_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/3dViewer.dir/Parser/parser.c.o"
+  "CMakeFiles/3dViewer.dir/Parser/parser.c.o.d"
+  "CMakeFiles/3dViewer.dir/Transformer/transformer.c.o"
+  "CMakeFiles/3dViewer.dir/Transformer/transformer.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/3dViewer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
